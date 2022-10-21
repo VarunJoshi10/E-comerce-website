c50dd5a33e44cbde8accd5e32728488bff865f68
Fusion-Pie <piyushhatewar@gmail.com>
2022-10-21 21:13:30 +0530

    some minor changes

--- E_commerce/onestop/views.py
+++ E_commerce/onestop/views.py
@@ -1,19 +1,13 @@
 from django.shortcuts import render, HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
+# For accessing social account model
+from allauth.socialaccount.models import SocialAccount
 
 # Create your views here.
 
 def hp(request):
     return render(request, 'main.html')
-
-    # user = User.objects()
-
-    # print(user)
-
-    # if User.is_authenticated():
-    #     return render(request, 'landing.html')
-
 
 def landing_page(request):
     return render(request, 'landing.html')
