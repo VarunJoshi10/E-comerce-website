42b29a2933fdb6dc65d496c5a609b03f2a8b5af4
Varun Joshi <72180614+VarunJoshi10@users.noreply.github.com>
2023-04-13 20:10:53 +0530

    Update settings.py

--- E_commerce/E_commerce/settings.py
+++ E_commerce/E_commerce/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-1vgf(%1)q!+%0-#cn$hne@3ljwo7(r6*ue1r67y66!%#hy9jrb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['20.119.16.29']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
