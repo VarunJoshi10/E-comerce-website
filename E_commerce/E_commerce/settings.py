dc8f74e4c588067a5a16e406758192fede9791d1
Varun Joshi <72180614+VarunJoshi10@users.noreply.github.com>
2023-04-14 12:21:27 +0530

    Update settings.py

--- E_commerce/E_commerce/settings.py
+++ E_commerce/E_commerce/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-1vgf(%1)q!+%0-#cn$hne@3ljwo7(r6*ue1r67y66!%#hy9jrb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['piyush-django.azurewebsites.net']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
